c462b81582c4fef73037890bbd081f14ee56bbb2
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-07-10 22:37:07 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "23abc"
+let score1 = null
+let score2 = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         //as its - "23abc" -> NaN | but if - "23" -> 23
+
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+
+console.log(valueInNumber1);    //0
+console.log(valueInNumber2);    //NaN
+
+let isLoggedIn = 1
+let valueIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(valueIsLoggedIn);
+// NOTE - if it is 0 or "" then it gives -> false other than this [1,85,"Shubh"] gives -> true
+
+let anyNumber = 25
+let stringAnyNumber = String(anyNumber)
+
+console.log(stringAnyNumber + " " + typeof stringAnyNumber);
