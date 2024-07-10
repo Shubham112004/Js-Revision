55875f7ee070e7aa5579b85ffc1713ee55c38f64
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-07-10 22:15:26 +0530

    datatypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";   //treat all js code as newer version
+
+/*
+primitive
+    number
+    bigint
+    string
+    boolean
+    null
+    undefined
+    symbol
+non-primitive
+    object
+    
+*/
+
+console.log(typeof undefined);     //undefined
+console.log(typeof null);          //object
