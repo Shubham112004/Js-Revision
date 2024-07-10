5a1ec4719fa645508734ad0d44569b35c9726e64
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-07-10 22:01:29 +0530

    varibles in js

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,15 @@
+const accountId = 15425
+let accountEmail = "shubhamgaikwad2k4@gmail.com"
+var accountPassword = "112004"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 2            //not allowed
+
+accountEmail = "sgaikwad2963@gmail.com"
+accountPassword = "122121"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
